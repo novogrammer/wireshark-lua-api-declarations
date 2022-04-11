c610d37e0394756f4646edfb1fc619622dcf536f
Yusuke Kawamoto <novogrammer@gmail.com>
2022-04-12 01:52:38 +0900

    few test

--- test/dist/test-all.lua
+++ test/dist/test-all.lua
@@ -1,2 +1,118 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-print("hello")
+-- Lua Library inline imports
+function __TS__Class(self)
+    local c = {prototype = {}}
+    c.prototype.__index = c.prototype
+    c.prototype.constructor = c
+    return c
+end
+
+function __TS__ArrayPush(arr, ...)
+    local items = {...}
+    for ____, item in ipairs(items) do
+        arr[#arr + 1] = item
+    end
+    return #arr
+end
+
+function __TS__ArrayFilter(arr, callbackfn)
+    local result = {}
+    do
+        local i = 0
+        while i < #arr do
+            if callbackfn(_G, arr[i + 1], i, arr) then
+                result[#result + 1] = arr[i + 1]
+            end
+            i = i + 1
+        end
+    end
+    return result
+end
+
+function __TS__New(target, ...)
+    local instance = setmetatable({}, target.prototype)
+    instance:____constructor(...)
+    return instance
+end
+
+function canCallNoError(f, ...)
+    local result = {pcall(f, ...)}
+    if not result[1] then
+        local ____ = result[2]
+    end
+    return result[1]
+end
+Counter = __TS__Class()
+Counter.name = "Counter"
+function Counter.prototype.____constructor(self)
+    self.hasCalledNoErrorResultList = {}
+end
+function Counter.prototype.tryPcall(self, name, f, ...)
+    local result = {pcall(f, ...)}
+    if result[1] then
+        __TS__ArrayPush(self.hasCalledNoErrorResultList, {isNoError = true, name = name})
+    else
+        __TS__ArrayPush(self.hasCalledNoErrorResultList, {isNoError = false, name = name, errorMessage = result[2]})
+    end
+end
+function Counter.prototype.printResult(self)
+    local calledWithErrorList = __TS__ArrayFilter(
+        self.hasCalledNoErrorResultList,
+        function(____, ____bindingPattern0)
+            local isNoError
+            isNoError = ____bindingPattern0.isNoError
+            return not isNoError
+        end
+    )
+    for ____, calledWithErrorOrNoError in ipairs(self.hasCalledNoErrorResultList) do
+        if not calledWithErrorOrNoError.isNoError then
+            print("error detail")
+            print(("name: \"" .. calledWithErrorOrNoError.name) .. "\"")
+            print(("errorMessage: \"" .. calledWithErrorOrNoError.errorMessage) .. "\"")
+        end
+    end
+    local totalCount = #self.hasCalledNoErrorResultList
+    local isNoErrorCount = #__TS__ArrayFilter(
+        self.hasCalledNoErrorResultList,
+        function(____, ____bindingPattern0)
+            local isNoError
+            isNoError = ____bindingPattern0.isNoError
+            return isNoError
+        end
+    )
+    print((("result " .. tostring(isNoErrorCount)) .. "/") .. tostring(totalCount))
+end
+counter = __TS__New(Counter)
+function test_chapter_11_1_1()
+    counter:tryPcall(
+        "Dumper.new",
+        function()
+            local dumper = Dumper.new("dummyfile")
+            dumper:close()
+        end
+    )
+    counter:tryPcall(
+        "Dumper.new with filetype",
+        function()
+            local dumper = Dumper.new("dummyfile", wtap_filetypes.PCAP)
+            dumper:close()
+        end
+    )
+    counter:tryPcall(
+        "Dumper.new with filetype encap",
+        function()
+            local dumper = Dumper.new("dummyfile", wtap_filetypes.PCAP, wtap_encaps.ETHERNET)
+            dumper:close()
+        end
+    )
+end
+function test_chapter_11_1()
+    test_chapter_11_1_1()
+end
+function test_all()
+    print("begin test_all")
+    test_chapter_11_1()
+    counter:printResult()
+    print("end test_all")
+end
+test_all()
--- test/src/test-all.ts
+++ test/src/test-all.ts
@@ -1,1 +1,94 @@
-console.log("hello");
+
+
+function canCallNoError<Args extends any[], R>(this: void, f: (this: void, ...args: Args) => R, ...args: Args): boolean {
+  const result = pcall(f, ...args);
+  if (!result[0]) {
+    result[1]
+  }
+  return result[0];
+}
+
+
+interface CalledWithError {
+  isNoError: true,
+  name: string,
+}
+
+interface CalledWithNoError {
+  isNoError: false,
+  name: string,
+  errorMessage: string,
+}
+type CalledWithErrorOrNoError = CalledWithError | CalledWithNoError;
+
+class Counter {
+
+  hasCalledNoErrorResultList: CalledWithErrorOrNoError[] = [];
+  constructor() {
+  }
+  tryPcall<Args extends any[], R>(name: string, f: (this: void, ...args: Args) => R, ...args: Args): void {
+    const result = pcall(f, ...args);
+    if (result[0]) {
+      this.hasCalledNoErrorResultList.push({
+        isNoError: true,
+        name,
+      });
+    } else {
+      this.hasCalledNoErrorResultList.push({
+        isNoError: false,
+        name,
+        errorMessage: result[1],
+      });
+    }
+  }
+  printResult() {
+
+    const calledWithErrorList=this.hasCalledNoErrorResultList.filter(({ isNoError }) => !isNoError);
+    for(let calledWithErrorOrNoError of this.hasCalledNoErrorResultList){
+      if(!calledWithErrorOrNoError.isNoError){
+        console.log("error detail");
+        console.log(`name: "${calledWithErrorOrNoError.name}"`);
+        console.log(`errorMessage: "${calledWithErrorOrNoError.errorMessage}"`);
+      }
+
+    }
+
+    const totalCount = this.hasCalledNoErrorResultList.length;
+    const isNoErrorCount = this.hasCalledNoErrorResultList.filter(({ isNoError }) => isNoError).length;
+    console.log(`result ${isNoErrorCount}/${totalCount}`);
+  }
+}
+
+const counter = new Counter();
+
+function test_chapter_11_1_1(this: void) {
+  counter.tryPcall("Dumper.new", () => {
+    const dumper = Dumper.new("dummyfile");
+    dumper.close();
+  });
+  counter.tryPcall("Dumper.new with filetype", () => {
+    const dumper = Dumper.new("dummyfile", wtap_filetypes.PCAP);
+    dumper.close();
+  });
+  counter.tryPcall("Dumper.new with filetype encap", () => {
+    const dumper = Dumper.new("dummyfile", wtap_filetypes.PCAP, wtap_encaps.ETHERNET);
+    dumper.close();
+  });
+}
+
+
+function test_chapter_11_1(this: void) {
+  test_chapter_11_1_1();
+}
+
+
+function test_all(this: void) {
+  console.log("begin test_all");
+  test_chapter_11_1();
+
+  counter.printResult();
+  console.log("end test_all");
+}
+
+
+test_all();
