3db3d7402be051cb53aea02f88ec9fb8b51328e1
Yusuke Kawamoto <novogrammer@gmail.com>
2022-04-13 03:02:10 +0900

    add *.pcap files

--- test/scripts/test-all.sh
+++ test/scripts/test-all.sh
@@ -4,4 +4,4 @@ CURRENT=$(cd $(dirname $0) && pwd)
 PARENT=$(cd $(dirname $0)/.. && pwd)
 # echo $(pwd)
 
-tshark -c 0 -X lua_script:$PARENT/dist/test_chapter_11_1.lua
+tshark -r $PARENT/captures/empty.pcap  -X lua_script:$PARENT/dist/test_chapter_11_1.lua
