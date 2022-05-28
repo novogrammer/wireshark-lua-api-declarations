6b5701435404b2a63483a3f015ea4f9bdf05e55b
Yusuke Kawamoto <novogrammer@gmail.com>
2022-05-29 03:56:00 +0900

    add test_chapter_11_2

--- test/dist/test_chapter_11_2.lua
+++ test/dist/test_chapter_11_2.lua
@@ -3,7 +3,245 @@ require("lualib_bundle");
 local ____exports = {}
 local ____Recorder = require("Recorder")
 local Recorder = ____Recorder.default
+local DUMMYFILE_PATH = "/tmp/dummyfile"
 local recorder = __TS__New(Recorder)
-print("TODO: test")
-recorder:printFailed()
+local field
+local function test_chapter_11_2_outside()
+    print("begin test_chapter_11_2_outside")
+    recorder:tryPcall(
+        "Field.new(fieldname)",
+        function()
+            field = Field.new("http.request")
+        end
+    )
+    print("end test_chapter_11_2_outside")
+end
+local function test_chapter_11_2_1()
+    recorder:tryPcall(
+        "Field.list()",
+        function()
+            local list = Field.list()
+        end
+    )
+    recorder:tryPcall(
+        "field:__call()",
+        function()
+            local fieldinfo = field()
+        end
+    )
+    recorder:tryPcall(
+        "field:__tostring()",
+        function()
+            local s = tostring(field)
+        end
+    )
+    recorder:tryPcall(
+        "field.name",
+        function()
+            local name = field.name
+            if name == nil then
+                error("name is null", 0)
+            end
+        end
+    )
+    recorder:tryPcall(
+        "field.display",
+        function()
+            local display = field.display
+            if display == nil then
+                error("display is null", 0)
+            end
+        end
+    )
+    recorder:tryPcall(
+        "field.type",
+        function()
+            local ____type = field.type
+            if ____type == nil then
+                error("type is null", 0)
+            end
+        end
+    )
+end
+local function test_chapter_11_2_2()
+    recorder:tryPcall(
+        "fieldinfo:__len()",
+        function()
+            local fieldinfo = field()
+            local length = #fieldinfo
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo:__unm()",
+        function()
+            local fieldinfo = field()
+            local unm = -fieldinfo
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo:__call()",
+        function()
+            local fieldinfo = field()
+            local value = fieldinfo()
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo:__tostring()",
+        function()
+            local fieldinfo = field()
+            local s = tostring(fieldinfo)
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo:__eq()",
+        function()
+            local fieldinfo = field()
+            local result = fieldinfo == fieldinfo
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo:__le()",
+        function()
+            local fieldinfo = field()
+            local result = fieldinfo <= fieldinfo
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo:__lt()",
+        function()
+            local fieldinfo = field()
+            local result = fieldinfo < fieldinfo
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.len",
+        function()
+            local fieldinfo = field()
+            local len = fieldinfo.len
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.offset",
+        function()
+            local fieldinfo = field()
+            local offset = fieldinfo.offset
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.value",
+        function()
+            local fieldinfo = field()
+            local value = fieldinfo.value
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.label",
+        function()
+            local fieldinfo = field()
+            local label = fieldinfo.label
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.display",
+        function()
+            local fieldinfo = field()
+            local display = fieldinfo.display
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.display",
+        function()
+            local fieldinfo = field()
+            local ____type = fieldinfo.type
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.source",
+        function()
+            local fieldinfo = field()
+            local source = fieldinfo.source
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.range",
+        function()
+            local fieldinfo = field()
+            local range = fieldinfo.range
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.generated",
+        function()
+            local fieldinfo = field()
+            local generated = fieldinfo.generated
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.hidden",
+        function()
+            local fieldinfo = field()
+            local hidden = fieldinfo.hidden
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.is_url",
+        function()
+            local fieldinfo = field()
+            local is_url = fieldinfo.is_url
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.little_endian",
+        function()
+            local fieldinfo = field()
+            local little_endian = fieldinfo.little_endian
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.big_endian",
+        function()
+            local fieldinfo = field()
+            local big_endian = fieldinfo.big_endian
+        end
+    )
+    recorder:tryPcall(
+        "fieldinfo.name",
+        function()
+            local fieldinfo = field()
+            local name = fieldinfo.name
+        end
+    )
+end
+local function test_chapter_11_2_3()
+    recorder:tryPcall(
+        "all_field_infos()",
+        function()
+            local result = {all_field_infos()}
+            if #result == nil then
+                error("length is null", 0)
+            end
+            if result[1].display == nil then
+                error("display is null", 0)
+            end
+        end
+    )
+end
+local function test_chapter_11_2()
+    print("begin test_chapter_11_2")
+    test_chapter_11_2_1()
+    test_chapter_11_2_2()
+    test_chapter_11_2_3()
+    print("end test_chapter_11_2")
+end
+test_chapter_11_2_outside()
+local chapter_11_2_proto = Proto.new("chapter_11_2_proto", "test")
+chapter_11_2_proto.init = function(self)
+end
+chapter_11_2_proto.dissector = function(buffer, pinfo, tree)
+    test_chapter_11_2()
+    recorder:printResult()
+    os.remove(DUMMYFILE_PATH)
+    return 0
+end
+register_postdissector(chapter_11_2_proto)
 return ____exports
--- test/src/test_chapter_11_2.ts
+++ test/src/test_chapter_11_2.ts
@@ -1,5 +1,180 @@
+
 import Recorder from "./Recorder";
 
+const DUMMYFILE_PATH = "/tmp/dummyfile";
+
 const recorder = new Recorder();
-console.log("TODO: test");
-recorder.printFailed();
+
+
+let field:Field;
+
+function test_chapter_11_2_outside(this:void){
+  console.log("begin test_chapter_11_2_outside");
+
+  recorder.tryPcall("Field.new(fieldname)", () => {
+    field = Field.new("http.request");
+  });
+  console.log("end test_chapter_11_2_outside");
+}
+
+function test_chapter_11_2_1(this: void) {
+  recorder.tryPcall("Field.list()", () => {
+    const list = Field.list();
+  });
+  recorder.tryPcall("field:__call()", () => {
+    const [fieldinfo] = field();
+  });
+  recorder.tryPcall("field:__tostring()", () => {
+    const s = field.toString();
+  });
+  recorder.tryPcall("field.name", () => {
+    // bug?
+    // const  name = field.name;
+    const  name = field["name"];
+    if(name==null){
+      throw "name is null";
+    }
+  });
+  recorder.tryPcall("field.display", () => {
+    const display = field.display;
+    if(display==null){
+      throw "display is null";
+    }
+  });
+  recorder.tryPcall("field.type", () => {
+    const type = field.type;
+    if(type==null){
+      throw "type is null";
+    }
+  });
+
+}
+function test_chapter_11_2_2(this: void) {
+  recorder.tryPcall("fieldinfo:__len()", () => {
+    const [fieldinfo] = field();
+    const length=fieldinfo.__len();
+  });
+  recorder.tryPcall("fieldinfo:__unm()", () => {
+    const [fieldinfo] = field();
+    const unm=fieldinfo.__unm();
+  });
+  recorder.tryPcall("fieldinfo:__call()", () => {
+    const [fieldinfo] = field();
+    const value=fieldinfo();
+  });
+  recorder.tryPcall("fieldinfo:__tostring()", () => {
+    const [fieldinfo] = field();
+    const s=fieldinfo.toString();
+  });
+  recorder.tryPcall("fieldinfo:__eq()", () => {
+    const [fieldinfo] = field();
+    const result = fieldinfo == fieldinfo;
+  });
+  recorder.tryPcall("fieldinfo:__le()", () => {
+    const [fieldinfo] = field();
+    const result = fieldinfo <= fieldinfo;
+  });
+  recorder.tryPcall("fieldinfo:__lt()", () => {
+    const [fieldinfo] = field();
+    const result = fieldinfo < fieldinfo;
+  });
+  recorder.tryPcall("fieldinfo.len", () => {
+    const [fieldinfo] = field();
+    const len = fieldinfo.len;
+  });
+  recorder.tryPcall("fieldinfo.offset", () => {
+    const [fieldinfo] = field();
+    const offset = fieldinfo.offset;
+  });
+  recorder.tryPcall("fieldinfo.value", () => {
+    const [fieldinfo] = field();
+    const value = fieldinfo.value;
+  });
+  recorder.tryPcall("fieldinfo.label", () => {
+    const [fieldinfo] = field();
+    const label = fieldinfo.label;
+  });
+  recorder.tryPcall("fieldinfo.display", () => {
+    const [fieldinfo] = field();
+    const display = fieldinfo.display;
+  });
+  recorder.tryPcall("fieldinfo.display", () => {
+    const [fieldinfo] = field();
+    const type = fieldinfo.type;
+  });
+  recorder.tryPcall("fieldinfo.source", () => {
+    const [fieldinfo] = field();
+    const source = fieldinfo.source;
+  });
+  recorder.tryPcall("fieldinfo.range", () => {
+    const [fieldinfo] = field();
+    const range = fieldinfo.range;
+  });
+  recorder.tryPcall("fieldinfo.generated", () => {
+    const [fieldinfo] = field();
+    const generated = fieldinfo.generated;
+  });
+  recorder.tryPcall("fieldinfo.hidden", () => {
+    const [fieldinfo] = field();
+    const hidden = fieldinfo.hidden;
+  });
+  recorder.tryPcall("fieldinfo.is_url", () => {
+    const [fieldinfo] = field();
+    const is_url = fieldinfo.is_url;
+  });
+  recorder.tryPcall("fieldinfo.little_endian", () => {
+    const [fieldinfo] = field();
+    const little_endian = fieldinfo.little_endian;
+  });
+  recorder.tryPcall("fieldinfo.big_endian", () => {
+    const [fieldinfo] = field();
+    const big_endian = fieldinfo.big_endian;
+  });
+  recorder.tryPcall("fieldinfo.name", () => {
+    const [fieldinfo] = field();
+    // bug?
+    // const name = fieldinfo.name;
+    const name = fieldinfo["name"];
+  });
+
+}
+function test_chapter_11_2_3(this: void) {
+  recorder.tryPcall("all_field_infos()", () => {
+    const result = all_field_infos();
+    if(result.length==null){
+      throw "length is null";
+    }
+    if(result[0].display==null){
+      throw "display is null";
+    }
+  });
+
+}
+
+
+function test_chapter_11_2(this: void) {
+  console.log("begin test_chapter_11_2");
+  test_chapter_11_2_1();
+  test_chapter_11_2_2();
+  test_chapter_11_2_3();
+  console.log("end test_chapter_11_2");
+}
+
+test_chapter_11_2_outside();
+
+
+const chapter_11_2_proto = Proto.new("chapter_11_2_proto", "test");
+chapter_11_2_proto.init = function () {
+
+}
+
+chapter_11_2_proto.dissector = function (this: void, buffer: Tvb, pinfo: Pinfo, tree: TreeItem): number {
+  test_chapter_11_2();
+  recorder.printResult();
+  os.remove(DUMMYFILE_PATH)
+  return 0;
+}
+
+register_postdissector(chapter_11_2_proto);
+
+
