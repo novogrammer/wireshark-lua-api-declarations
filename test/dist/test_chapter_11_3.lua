6a4194bd490b6cac88079e7b7fc338880feb60cf
Yusuke Kawamoto <novogrammer@gmail.com>
2022-05-29 04:49:52 +0900

    add test for wireshark

--- test/dist/test_chapter_11_3.lua
+++ test/dist/test_chapter_11_3.lua
@@ -4,6 +4,46 @@ local ____exports = {}
 local ____Recorder = require("Recorder")
 local Recorder = ____Recorder.default
 local recorder = __TS__New(Recorder)
-print("TODO: test")
-recorder:printFailed()
+local function test_chapter_11_3_1()
+    recorder:tryPcall(
+        "foo",
+        function()
+            error("todo", 0)
+        end
+    )
+end
+local function test_chapter_11_3_2()
+    recorder:tryPcall(
+        "foo",
+        function()
+            error("todo", 0)
+        end
+    )
+end
+local function test_chapter_11_3_3()
+    recorder:tryPcall(
+        "gui_enabled()",
+        function()
+            local enabled = gui_enabled()
+        end
+    )
+    recorder:tryPcall(
+        "foo",
+        function()
+            error("todo", 0)
+        end
+    )
+end
+local function test_chapter_11_3()
+    print("begin test_chapter_11_3")
+    test_chapter_11_3_1()
+    test_chapter_11_3_2()
+    test_chapter_11_3_3()
+    print("end test_chapter_11_3")
+end
+test_chapter_11_3()
+recorder:printResult()
+if gui_enabled() then
+    os.exit()
+end
 return ____exports
--- test/src/test_chapter_11_3.ts
+++ test/src/test_chapter_11_3.ts
@@ -1,5 +1,44 @@
+
 import Recorder from "./Recorder";
 
 const recorder = new Recorder();
-console.log("TODO: test");
-recorder.printFailed();
+
+
+function test_chapter_11_3_1(this: void) {
+  recorder.tryPcall("foo", () => {
+    throw "todo";
+  });
+
+}
+function test_chapter_11_3_2(this: void) {
+  recorder.tryPcall("foo", () => {
+    throw "todo";
+  });
+
+}
+function test_chapter_11_3_3(this: void) {
+  recorder.tryPcall("gui_enabled()", () => {
+    const enabled = gui_enabled();
+  });
+  recorder.tryPcall("foo", () => {
+    throw "todo";
+  });
+
+}
+
+
+function test_chapter_11_3(this: void) {
+  console.log("begin test_chapter_11_3");
+  test_chapter_11_3_1();
+  test_chapter_11_3_2();
+  test_chapter_11_3_3();
+  console.log("end test_chapter_11_3");
+}
+
+test_chapter_11_3();
+recorder.printResult();
+if(gui_enabled()){
+  //exit wireshark
+  os.exit();
+}
+
