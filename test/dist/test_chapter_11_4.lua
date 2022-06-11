b327518a2348c264705723b50f3ed626f4dce35f
Yusuke Kawamoto <novogrammer@gmail.com>
2022-06-11 19:29:31 +0900

    add test_chapter_11_4

--- test/dist/test_chapter_11_4.lua
+++ test/dist/test_chapter_11_4.lua
@@ -4,6 +4,97 @@ local ____exports = {}
 local ____Recorder = require("Recorder")
 local Recorder = ____Recorder.default
 local recorder = __TS__New(Recorder)
-print("TODO: test")
-recorder:printFailed()
+local function test_chapter_11_4_outside()
+end
+local function test_chapter_11_4_1()
+    recorder:tryPcall(
+        "Listener.new()",
+        function()
+            local listener = Listener.new()
+        end
+    )
+    recorder:tryPcall(
+        "Listener.new(tap)",
+        function()
+            local listener = Listener.new("http")
+        end
+    )
+    recorder:tryPcall(
+        "Listener.new(nil,filter)",
+        function()
+            local listener = Listener.new(nil, "m2tp")
+        end
+    )
+    recorder:tryPcall(
+        "Listener.new(nil,nil,allfields)",
+        function()
+            local listener = Listener.new(nil, nil, true)
+        end
+    )
+    recorder:tryPcall(
+        "Listener.list()",
+        function()
+            local list = Listener.list()
+        end
+    )
+    recorder:tryPcall(
+        "listener:remove",
+        function()
+            local listener = Listener.new()
+            listener:remove()
+        end
+    )
+    recorder:tryPcall(
+        "listener:__tostring",
+        function()
+            local listener = Listener.new()
+            tostring(listener)
+        end
+    )
+    recorder:tryPcall(
+        "listener:packet",
+        function()
+            local listener = Listener.new()
+            listener.packet = function(pinfo, tvb, tapinfo)
+                print("hello listener:packet!")
+                print("pinfo.src: " .. tostring(pinfo.src))
+                print("tvb.len(): " .. tostring(tvb:len()))
+                print("tapinfo: " .. tostring(tapinfo == nil and "null" or pairs(tapinfo)))
+            end
+        end
+    )
+    recorder:tryPcall(
+        "listener:draw",
+        function()
+            local listener = Listener.new()
+            listener.draw = function()
+                print("hello listener:draw!")
+            end
+        end
+    )
+    recorder:tryPcall(
+        "listener:reset",
+        function()
+            local listener = Listener.new()
+            listener.reset = function()
+                print("hello listener:reset!")
+            end
+        end
+    )
+end
+local function test_chapter_11_4()
+    print("begin test_chapter_11_4")
+    test_chapter_11_4_1()
+    print("end test_chapter_11_4")
+end
+test_chapter_11_4_outside()
+local chapter_11_2_proto = Proto.new("chapter_11_2_proto", "test")
+chapter_11_2_proto.init = function(self)
+end
+chapter_11_2_proto.dissector = function(buffer, pinfo, tree)
+    test_chapter_11_4()
+    recorder:printResult()
+    return 0
+end
+register_postdissector(chapter_11_2_proto)
 return ____exports
--- test/src/test_chapter_11_4.ts
+++ test/src/test_chapter_11_4.ts
@@ -1,5 +1,91 @@
+
 import Recorder from "./Recorder";
 
 const recorder = new Recorder();
-console.log("TODO: test");
-recorder.printFailed();
+
+
+// let field:Field;
+
+function test_chapter_11_4_outside(this:void){
+  // console.log("begin test_chapter_11_4_outside");
+
+  // recorder.tryPcall("Field.new(fieldname)", () => {
+  //   field = Field.new("http.request");
+  // });
+  // console.log("end test_chapter_11_4_outside");
+}
+
+function test_chapter_11_4_1(this: void) {
+  recorder.tryPcall("Listener.new()", () => {
+    const listener=Listener.new();
+  });
+  recorder.tryPcall("Listener.new(tap)", () => {
+    const listener=Listener.new("http");
+  });
+  recorder.tryPcall("Listener.new(nil,filter)", () => {
+    const listener=Listener.new(undefined,"m2tp");
+  });
+  recorder.tryPcall("Listener.new(nil,nil,allfields)", () => {
+    const listener=Listener.new(undefined,undefined,true);
+  });
+  recorder.tryPcall("Listener.list()", () => {
+    const list=Listener.list();
+    // for(let [_,tapname] of pairs(list)){
+    //   console.log(_,tapname);
+    // }
+  });
+  recorder.tryPcall("listener:remove", () => {
+    const listener=Listener.new();
+    listener.remove();
+  });
+  recorder.tryPcall("listener:__tostring", () => {
+    const listener=Listener.new();
+    tostring(listener);
+  });
+  recorder.tryPcall("listener:packet", () => {
+    const listener=Listener.new();
+    listener.packet=(pinfo:Pinfo,tvb:Tvb,tapinfo:LuaTable | null)=>{
+      console.log("hello listener:packet!");
+      console.log(`pinfo.src: ${pinfo.src}`);
+      console.log(`tvb.len(): ${tvb.len()}`);
+      console.log(`tapinfo: ${tapinfo==null?"null":pairs(tapinfo)}`);
+    };
+  });
+  recorder.tryPcall("listener:draw", () => {
+    const listener=Listener.new();
+    listener.draw=()=>{
+      console.log("hello listener:draw!");
+    }
+  });
+  recorder.tryPcall("listener:reset", () => {
+    const listener=Listener.new();
+    listener.reset=()=>{
+      console.log("hello listener:reset!");
+    }
+  });
+}
+
+
+function test_chapter_11_4(this: void) {
+  console.log("begin test_chapter_11_4");
+  test_chapter_11_4_1();
+  console.log("end test_chapter_11_4");
+}
+
+test_chapter_11_4_outside();
+
+
+const chapter_11_2_proto = Proto.new("chapter_11_2_proto", "test");
+chapter_11_2_proto.init = function () {
+
+}
+
+chapter_11_2_proto.dissector = function (this: void, buffer: Tvb, pinfo: Pinfo, tree: TreeItem): number {
+  test_chapter_11_4();
+  recorder.printResult();
+  return 0;
+}
+
+register_postdissector(chapter_11_2_proto);
+
+
