301243f72e4dcb773026724baff04feef74b7df0
Yusuke Kawamoto <novogrammer@gmail.com>
2022-09-10 06:51:29 +0900

    test_chaper_11_14

--- test/dist/test_chapter_11_14.lua
+++ test/dist/test_chapter_11_14.lua
@@ -4,6 +4,104 @@ local ____exports = {}
 local ____Recorder = require("Recorder")
 local Recorder = ____Recorder.default
 local recorder = __TS__New(Recorder)
-print("TODO: test")
-recorder:printFailed()
+local function test_chapter_11_14_1()
+    recorder:tryPcall(
+        "Struct.pack(format, value)",
+        function()
+            do
+                local result, pos = Struct.pack("I", 67324752)
+                if type(result) ~= "string" then
+                    error("not string", 0)
+                end
+                if type(pos) ~= "nil" then
+                    error("not nil", 0)
+                end
+            end
+            do
+                local result, pos = Struct.pack("I=I", 67324752, 67324752)
+                if type(result) ~= "string" then
+                    error("not string", 0)
+                end
+                if type(pos) ~= "number" then
+                    error("not number", 0)
+                end
+            end
+        end
+    )
+    recorder:tryPcall(
+        "Struct.unpack(format, struct)",
+        function()
+            local packed = Struct.pack("I", 67324752)
+            local result = Struct.unpack("I", packed)
+            if type(result) ~= "number" then
+                error("not number", 0)
+            end
+        end
+    )
+    recorder:tryPcall(
+        "Struct.unpack(format, struct, begin)",
+        function()
+            local packed = Struct.pack("I", 67324752)
+            local result = Struct.unpack("I", packed, 1)
+            if type(result) ~= "number" then
+                error("not number", 0)
+            end
+        end
+    )
+    recorder:tryPcall(
+        "Struct.size(format)",
+        function()
+            local result = Struct.size("I")
+            if type(result) ~= "number" then
+                error("not number", 0)
+            end
+        end
+    )
+    recorder:tryPcall(
+        "Struct.values(format)",
+        function()
+            local result = Struct.values("I")
+            if type(result) ~= "number" then
+                error("not number", 0)
+            end
+        end
+    )
+    recorder:tryPcall(
+        "Struct.tohex(bytestring)",
+        function()
+            local packed = Struct.pack("I", 67324752)
+            local result = Struct.tohex(packed)
+            if type(result) ~= "string" then
+                error("not string", 0)
+            end
+        end
+    )
+    recorder:tryPcall(
+        "Struct.tohex(bytestring,lowercase)",
+        function()
+            local packed = Struct.pack("I", 67324752)
+            local result = Struct.tohex(packed, true)
+            if type(result) ~= "string" then
+                error("not string", 0)
+            end
+        end
+    )
+    recorder:tryPcall(
+        "Struct.tohex(bytestring,nil,separator)",
+        function()
+            local packed = Struct.pack("I", 67324752)
+            local result = Struct.tohex(packed, nil, " ")
+            if type(result) ~= "string" then
+                error("not string", 0)
+            end
+        end
+    )
+end
+local function test_chapter_11_14()
+    print("begin test_chapter_11_14")
+    test_chapter_11_14_1()
+    print("end test_chapter_11_14")
+end
+test_chapter_11_14()
+recorder:printResult()
 return ____exports
--- test/src/test_chapter_11_14.ts
+++ test/src/test_chapter_11_14.ts
@@ -1,5 +1,87 @@
 import Recorder from "./Recorder";
 
+
 const recorder = new Recorder();
-console.log("TODO: test");
-recorder.printFailed();
+
+function test_chapter_11_14_1(this: void) {
+
+  recorder.tryPcall("Struct.pack(format, value)", () => {
+    {
+      const [result,pos] = Struct.pack("I", 67324752);
+      if(type(result)!="string"){
+        throw "not string";
+      }
+      if(type(pos)!="nil"){
+        throw "not nil";
+      }
+    }
+    {
+      const [result,pos] = Struct.pack("I=I", 67324752, 67324752);
+      if(type(result)!="string"){
+        throw "not string";
+      }
+      if(type(pos)!="number"){
+        throw "not number";
+      }
+    }
+  });
+  recorder.tryPcall("Struct.unpack(format, struct)", () => {
+    const [packed] = Struct.pack("I", 67324752);
+
+    const [result]=Struct.unpack("I",packed);
+    if(type(result)!="number"){
+      throw "not number";
+    }
+  });
+  recorder.tryPcall("Struct.unpack(format, struct, begin)", () => {
+    const [packed] = Struct.pack("I", 67324752);
+
+    const [result]=Struct.unpack("I",packed,1);
+    if(type(result)!="number"){
+      throw "not number";
+    }
+  });
+  recorder.tryPcall("Struct.size(format)", () => {
+    const result = Struct.size("I");
+    if(type(result)!="number"){
+      throw "not number";
+    }
+  });
+  recorder.tryPcall("Struct.values(format)", () => {
+    const result = Struct.values("I");
+    if(type(result)!="number"){
+      throw "not number";
+    }
+  });
+  recorder.tryPcall("Struct.tohex(bytestring)", () => {
+    const [packed] = Struct.pack("I", 67324752);
+    const result = Struct.tohex(packed);
+    if(type(result)!="string"){
+      throw "not string";
+    }
+  });
+  recorder.tryPcall("Struct.tohex(bytestring,lowercase)", () => {
+    const [packed] = Struct.pack("I", 67324752);
+    const result = Struct.tohex(packed,true);
+    if(type(result)!="string"){
+      throw "not string";
+    }
+  });
+  recorder.tryPcall("Struct.tohex(bytestring,nil,separator)", () => {
+    const [packed] = Struct.pack("I", 67324752);
+    const result = Struct.tohex(packed,undefined," ");
+    if(type(result)!="string"){
+      throw "not string";
+    }
+  });
+
+}
+
+function test_chapter_11_14(this: void) {
+  console.log("begin test_chapter_11_14");
+  test_chapter_11_14_1();
+  console.log("end test_chapter_11_14");
+}
+
+test_chapter_11_14();
+recorder.printResult();
